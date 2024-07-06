0212deac5197ce8e1b9d2a57727c9faa8469d8ad
parkSeryu <pjh971212@gmail.com>
2024-07-07 01:26:58 +0900

    Business Layer 테스트 (3)

--- src/main/java/sample/cafekiosk/spring/api/service/order/OrderService.java
+++ src/main/java/sample/cafekiosk/spring/api/service/order/OrderService.java
@@ -28,6 +28,7 @@ public class OrderService {
 
     /**
      * 재고 감소 -> 동시성 고민
+     * optimistic lock / pessimistic lock / ...
      */
     public OrderResponse createOrder(OrderCreateRequest request, LocalDateTime registeredDateTime) {
         List<String> productNumbers = request.getProductNumbers();
