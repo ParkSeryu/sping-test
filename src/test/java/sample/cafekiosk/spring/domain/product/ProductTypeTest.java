276d73a684f502eb5917bfb23b47af8f1c6db2ad
parkSeryu <pjh971212@gmail.com>
2024-07-11 23:01:36 +0900

    @ParameterizedTest

--- src/test/java/sample/cafekiosk/spring/domain/product/ProductTypeTest.java
+++ src/test/java/sample/cafekiosk/spring/domain/product/ProductTypeTest.java
@@ -4,6 +4,8 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 class ProductTypeTest {
 
@@ -33,6 +35,42 @@ class ProductTypeTest {
 
         // then
         assertThat(result).isTrue();
+    }
+
+    @DisplayName("삼품 타입이 재고 관련 타입인지를 체크한다.")
+    @Test
+    void containsStockType3() {
+        // given
+        ProductType givenType1 = ProductType.HANDMADE;
+        ProductType givenType2 = ProductType.BOTTLE;
+        ProductType givenType3 = ProductType.BAKERY;
+
+        // when
+        boolean result1 = ProductType.containsStockType(givenType1);
+        boolean result2 = ProductType.containsStockType(givenType2);
+        boolean result3 = ProductType.containsStockType(givenType3);
+
+
+        // then
+        assertThat(result1).isFalse();
+        assertThat(result2).isTrue();
+        assertThat(result3).isTrue();
+    }
+
+    @DisplayName("삼품 타입이 재고 관련 타입인지를 체크한다.")
+    @CsvSource({
+            "HANDMADE, false",
+            "BOTTLE, true",
+            "BAKERY, true"
+    })
+    @ParameterizedTest
+    void containsStockType4(ProductType productType, boolean expected) {
+        // when
+        boolean result = ProductType.containsStockType(productType);
+
+
+        // then
+        assertThat(result).isEqualTo(expected);
     }
 
 }
